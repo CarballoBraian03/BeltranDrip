db4930251ad0a050ce74d6e34cff7bf4ff7c585d
CarballoBraian03 <Briangab4496@hotmail.com>
2024-10-13 13:27:00 -0300

    Delete TP.BeltranDrip/Beltran Drip con footer(ultimo.1)/Beltran Drip con footer(ultimo)/Beltran Drip/Pagina directory

--- TP.BeltranDrip/Beltran Drip con footer(ultimo.1)/Beltran Drip con footer(ultimo)/Beltran Drip/Pagina/funcion.js
+++
@@ -1,9 +0,0 @@
-function VerContra(){
-        var usuario = document.getElementById("txtContraseña");
-        if (usuario.type === "password") {
-            usuario.type = "text";  
-        } else {
-            usuario.type = "password";  
-        }
-    }
-    
